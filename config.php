2c845ea14598c42e8d6e61e5fad66e0fe64aabea
Rizky Kurnia Antasari <rizkypro27.co@gmail.com>
2025-05-31 22:13:07 +0700

    nambahin config

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');   
+define('DB_NAME', 'web_kampus');
+
+define('BASE_URL', 'http://localhost/project_web_kampus/'); 
+?>
